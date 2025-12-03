f8a1a2820a74261f08c6ae6b3d3ee7bcd0708f63
chiscookeke11 <chiscookeke11@gmail.com>
2025-12-03 08:47:53 +0100

    added instagram link

--- katsina_youth_craft/public/data/socialLinks.tsx
+++ katsina_youth_craft/public/data/socialLinks.tsx
@@ -2,25 +2,25 @@
 
 
 
-import { Facebook, Linkedin, Twitter} from "lucide-react";
+import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 
 
 export const socialsData = [
-    {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/company/thegreyit/",
-        icon: <Linkedin size={20}/>
-    },
+  {
+    name: "LinkedIn",
+    url: "https://www.instagram.com/kycv28?igsh=MXU1YW9ndmVrbDJ6bA==",
+    icon: <Instagram size={20} />
+  },
 
-      {
-        name: "Facebook",
-        url: "https://web.facebook.com/Thegreyit",
-        icon: <Facebook size={20}/>
-    },
+  {
+    name: "Facebook",
+    url: "/",
+    icon: <Facebook size={20} />
+  },
 
-      {
-        name: "X",
-        url: "https://x.com/thegreyit",
-        icon: <Twitter size={20}/>
-    },
+  {
+    name: "X",
+    url: "/",
+    icon: <Twitter size={20} />
+  },
 ]
